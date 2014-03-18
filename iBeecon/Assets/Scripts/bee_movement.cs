0fd4089ccee1607d655cf9c12f2eace1e30ff396
Foteini Bogiatzi <foteini@student.chalmers.se>
2014-03-18 16:41:21 +0100

    empty script fix

--- iBeecon/Assets/Scripts/bee_movement.cs
+++ iBeecon/Assets/Scripts/bee_movement.cs
@@ -5,11 +5,13 @@ public class bee_movement : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		//float speed = 2.0f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		//transform.position += transform.forward * speed * Time.deltaTime;
+		transform.Translate(5f * Time.deltaTime, 0f, 0f);
 	}
+
 }
