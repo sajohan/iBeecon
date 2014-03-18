a23d45e79d285cbd3686d1471648272bfe6f5035
Foteini Bogiatzi <foteini@student.chalmers.se>
2014-03-18 17:50:26 +0100

    Made background into a separate layer

--- iBeecon/Assets/Components/BeeMovement.cs
+++ iBeecon/Assets/Components/BeeMovement.cs
@@ -2,15 +2,16 @@
 using System.Collections;
 
 public class BeeMovement : MonoBehaviour {
+	float speed = 2.0f;
 
 	// Use this for initialization
 	void Start () {
-		//float speed = 2.0f;
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		//transform.position += transform.forward * speed * Time.deltaTime;
+		transform.position += transform.forward * speed * Time.deltaTime;
 		transform.Translate(5f * Time.deltaTime, 0f, 0f);
 	}
 
